a47033aa807113a7f2bb5c9eed07bb42949096d1
Jim Holmström <jim.holmstroem@gmail.com>
2015-01-16 16:00:50 +0100

    forced port 5000

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+
 from flask import Flask, request
-
-
 app = Flask('EchoServer')
 
 
@@ -9,8 +9,7 @@ def echo():
     return request.get_data()
 
 if __name__ == '__main__':
-    """
-    usage: python main.py <port>
-    """
-    import sys
-    app.run(port=int(sys.argv[1]))
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+    )
